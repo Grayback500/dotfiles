15fabcb8eadaa5984091cdf63fa009f34571e7b9
Isaiah <isaiah.fonville19@gmail.com>
2025-08-29 16:40:24 -0500

    Add Windows Doom Emacs config

---
+++ doom-emacs/.doom.d/custom.el
@@ -0,0 +1,51 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738"
+     "5bf50e9325062d35f1a304a97be13b16ce240ea2721ca9851c6584cff3aed81c"
+     "4aee0a939c49baa99948d9f685c134a274f3e6a46e23b76fe30dc0ddc9b0748c"
+     "a1abf0a3e85adcf8da321d3c7f98f85a120cd50ff5dcfe812a647662f04ad0b2"
+     "7e5e90061f3931f4b29b977a61a742ab45b7b01049eea4a1fb6fdc042cb842b2"
+     "79b922b0d10d379c8d0e8db0f05f0eec98ed4a50cb7e199c97b0369d19e89d49"
+     "2de4de62b6fd7a070e549d641f810e873647094f23492a42d71e31a947fec7cb"
+     "8de91e43d21b9d5cb79a146b267d29d3e377ed73888324912c7f6e514a76e5d7"
+     "7dd0d52008c74610c4ae8f539246d18b5db3cca9463fd4b186bdcd1e8a7782a8"
+     "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392"
+     "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
+     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
+     "032f0de3d3eba65c9f561ec788b029488074c6d66cd025c10be3bba359c0dd2c"
+     "0cc70543214e5133e0eb479a01e57128a4f3e62195ca9073dffe90c8a57519e1"
+     "8ba8918be4bb12c57cae812f8f9543e7a4b59a3cc1d5d4a4f97dc26a397c94e3"
+     "7fb25688a73234177e66a36b5c8178feed7da7a970d4464f51a0165ffcbf1286"
+     "377bf88f6a5c5085bccdd96beeade170a55df7944e9768f1108ada15d4e71e02"
+     "6a5584ee8de384f2d8b1a1c30ed5b8af1d00adcbdcd70ba1967898c265878acf"
+     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0"
+     "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700"
+     "113a135eb7a2ace6d9801469324f9f7624f8c696b72e3709feb7368b06ddaccc"
+     "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e"
+     "e14884c30d875c64f6a9cdd68fe87ef94385550cab4890182197b95d53a7cf40"
+     "77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d"
+     "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
+     "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882"
+     "2771ec93656faf267521dce9ffe1a6ad88cd0bea87aa0e8c4fc80bf355c58c1d"
+     "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0"
+     "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f"
+     "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725"
+     "dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176" default))
+ '(org-agenda-files
+   '("~/org/fa2025/fall25.org" "/home/saint/org/Getting Started with Orgzly.org"))
+ '(package-selected-packages
+   '(autothemer autumn-light-theme base16-theme company-jedi jazz-theme
+     light-soap-theme solo-jazz-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
