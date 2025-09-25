31e9835a69e00bf6de5999913ff092e05d3b7006
Isaiah <Isaiah.fonville19@gmail.com>
2025-09-25 09:41:50 -0500

    Changed theme colors for kitty, i3, polybar, and rofi from violet to green, to match the Artist's garden at Eragany wallpaper

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# Hyprland
-mkdir -p ~/.config/hypr
-ln -sf ~/dotfiles/hyprland/* ~/.config/hypr/
+# i3
+ln -sf ~/dotfiles/i3 ~/.config/i3/
 
 # Doom Emacs
 ln -sf ~/dotfiles/doom-emacs/* ~/.doom.d/
 
 # Kitty
 ln -sf ~/dotfiles/kitty/ ~/.config/kitty/
+
+# rofi
+ln -sf ~/dotfiles/rofi ~/.config/rofi/
+
+# picom
+ln -sf ~/dotfiles/picom ~/.config/picom
+
+# polybar
+ln -sf ~/dotfiles/polybar ~/.config/polybar
